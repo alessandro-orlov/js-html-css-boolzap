06382c31969fd26569a8ed1884d94f70c82beda2
Alessandro  (Notebook) <alex.ixs@hotmail.com>
2020-06-21 15:07:22 +0200

    Final touch
    Aggiunto funzione switch case per la risposta, aggiunti commenti nel JS, aggiunto input focus con JS sul input

--- js/myscript.js
+++ js/myscript.js
@@ -27,8 +27,7 @@ $(document).ready(
         // Sostituisco l'orraio dell'ultima visita
         $('.active-contact').find('.last-time-seen').text(lastTimeSeen);
 
-        // Aggiungo classe active al contatto cliccato e lo rimuovo a quello
-        // attualmente attivo
+        // Aggiungo classe active al contatto cliccato e lo rimuovo a quello attualmente attivo
         $(this).siblings().removeClass('active');
         $(this, 'li.contact-js').addClass('active');
     });
@@ -44,21 +43,26 @@ $(document).ready(
         var dataContact = $(this).attr('data-contact');
         var dataChat = '.chat-saved[data-chat="'+ dataContact +'"]';
 
+        // Aggiungo classe visible alla chat del contatto cliccato
         $(dataChat).addClass('visible');
+        // Rimuovo classe visibile a tutti gli altri
         $(dataChat).siblings().removeClass('visible');
-
     });
 
 
-    // ===================================================
-    // =========== INVIO IL MESSAGGIO ALLA CHAT ==========
+    // =====================================================
+    // =========== INVIO IL MESSAGGIO ALLA CHAT ============
 
+    // ================ BUTTON ==============
     // Al click sul pulasnte prendo il valore del input
     $('button.add-message-js').click(function() {
       // Prendo il valore per il messagio da stampare
       var messaggioVal = $('.new-msg').val();
-      var response = 'ok';
 
+      // Risposta in base al valore dell'input con la funzione(line: 255)
+      var response = responseVariant(messaggioVal);
+
+      // Prendo l'orario corrente con la funzione (line: 224)
       var lastSeenTime = getCurrentTime();
 
       // TAMPLATE
@@ -67,32 +71,34 @@ $(document).ready(
 
       // Aggiungo il messaggio tamplate alla chat
       if( messaggioVal !='' ) {
-        // Stampo il messagio nella chat window (riga JS 128)
+        // Stampo il messagio nella chat window message function (line: 128)
         message(messaggioVal, messageTamplate);
 
-        temporalResponse(500, 3500, lastSeenTime);
+        // Dopo 0.5s Visualizzo che si sta digitando la risposta per 3.5s
+        typingResponse(500, 3500, lastSeenTime);
 
-        // Ricevo la risposta dopo 3.5s
+        // Stampo il messaggio di risposta dopo 3.5s
         setTimeout(function() {
           message(response, responseTamplate);
 
           // Cambio il tempo dell'ultima visita del contatto attivo
           $('li.contact-js.active').find('.time-stamp').text(lastSeenTime);
 
-
         }, 3500);
-
-
-
 
       } // End if
     }); // End click on button event
 
-    // Invio il messagio alla chat premendo l'invio (enter sulla tastiera)
+    // ================ ENTER ==================
+    // Invio il messagio alla chat premendo l'invio
     $('.new-msg').keypress(function(event) {
       // Prendo il valore per il messagio da stampare
       var messaggioVal = $('.new-msg').val();
-      var response = 'Ciao';
+
+      // Risposta in base al valore dell'input con la funzione(line: 255)
+      var response = responseVariant(messaggioVal);
+
+      // Inserisco l'orario corrente del messagio
       var lastSeenTime = getCurrentTime();
 
       // TAMPLATE
@@ -103,9 +109,10 @@ $(document).ready(
         // Stampo il messagio nella chat-window
         message(messaggioVal, messageTamplate);
 
-        temporalResponse(500, 2500, lastSeenTime);
+        // Dopo 0.5s Visualizzo che si sta digitando la risposta per 2.5s
+        typingResponse(500, 2500, lastSeenTime);
 
-        // Ricevo la risposta dopo 1.5sW
+        // Ricevo la risposta dopo 2.5s
         setTimeout(function() {
           message(response, responseTamplate);
 
@@ -114,6 +121,7 @@ $(document).ready(
 
         }, 2500);
       } // End if
+
 
     }); // End keypress event
 
@@ -145,7 +153,20 @@ $(document).ready(
      // Invoco la funzione di ricerca (riga JS 185)
      searcContactName();
 
-
+     // ==========================================
+     // ============ Focus sul input =============
+     $('.write-message input.new-msg').focus(
+       function() {
+         $('.send-normal-msg').addClass('active');
+         $('.send-audio-msg').removeClass('active');
+       }
+     );
+     $('.write-message input.new-msg').blur(
+       function() {
+         $('.send-normal-msg').removeClass('active');
+         $('.send-audio-msg').addClass('active');
+       }
+     );
   }); // End document ready
 
 
@@ -160,7 +181,7 @@ function message(text, tamplate) {
   tamplate.find('.msg-text').text(text);
   tamplate.find('.msg-send-time').replaceWith(responseTime);
 
-  // Aggiungo il messaggi di risposta alla chat
+  // Aggiungo il messaggi di risposta alla chat attualmente visualizzata ('.visible)
   $('.chat-window .visible').append(tamplate);
 
   // Resetto il valore iniziale (vuoto) dell'input
@@ -172,7 +193,7 @@ function message(text, tamplate) {
 }
 
 // Funzione risposta temporanea
-function temporalResponse(durationTime, afterTime, timeSeen) {
+function typingResponse(durationTime, afterTime, timeSeen) {
   setTimeout( function() {
     // Cambio la scritta per un tot di millesecondi (durationTime)
     $('.contact-last-seen').text('Sta scrivendo ... ')
@@ -222,16 +243,56 @@ function temporalResponse(durationTime, afterTime, timeSeen) {
   // Funzione di ricerca per il nome dell'utente o chat
   function searcContactName() {
     $('.search-contact').keyup( function() {
-      var searchContact = $('input.search-contact').val().toLowerCase();
+      // Inserisco il valore da cercare
+      var searchQuery = $('input.search-contact').val().toLowerCase();
+
+      // Lista di tutti i contati
       var allContacts = $('li.contact-js');
 
+      // Il ciclo di verifica per tutti i contatti
       allContacts.each( function() {
+
+        // Seleziono il nome del contatto ('.contact-name')
         var contactName = $(this).find('.contact-name').text().toLowerCase();
-        if( contactName.includes(searchContact) ) {
+
+        if( contactName.includes(searchQuery) ) {
+          // Viusalizzo il contatto se contiene il valore inserito nella search
           $(this).show();
         } else {
+          // Nascondo altri contatti
           $(this).hide();
         }
       }); // End each
     }); // End keyup function
   } // End searcContactName() function
+
+
+  // Funzione per la risposta - stupid & minimal AI :D
+  function responseVariant(value) {
+    var response;
+
+    // Modifico la risposta se il nel messaggio inviato dall'utente è presente un determinato carattere o parola
+    switch (true) {
+      case value.includes('?'), value.includes('musica'):
+        response = 'Adoro la musica';
+        break;
+      case value.includes('!'):
+        response = 'Si, ma stai calmo';
+        break;
+      case value.includes('ciao'), value.includes('mondo'):
+        response = 'Hello World';
+        break;
+      case value.includes('ciao') || value.includes('buongiorno') :
+        response = 'Ehila';
+        break;
+      case value.includes('?'):
+        response = 'che?';
+        break;
+      case value.includes('ok'):
+        response = 'okey';
+        break;
+      default:
+        response = 'ok :)';
+    }
+    return response;
+  };
